4049af0ff9102ed1f7d05c7d08a45c93cdbb18c6
ReallyGoodCoder1234 <andydai.inbox@gmail.com>
2021-09-15 08:21:34 +1000

    I added a release ghost function

--- Pac_man.py
+++ Pac_man.py
@@ -3,45 +3,43 @@ import pygame, sys, random
 from pygame.locals import (K_DOWN, K_UP, K_UP, K_LEFT, K_RIGHT, K_a, K_d, K_s, K_w, RLEACCEL)
 
 class Ghost(pygame.sprite.Sprite):
+    global ghost_colours
     def __init__(self):
         super(Ghost, self).__init__()
         imageName = random.choice(ghost_list)
-        image = "./Assets/Sprites/Ghosts/" + imageName + "/" + imageName + "_forward.png"
-        print(image)
-        self.surf = pygame.image.load(image).convert()
+        while ghost_colours.count(imageName) != 0:
+           imageName = random.choice(ghost_list)
+
+        self.surf = pygame.image.load("./Assets/Sprites/Ghosts/" + imageName + "/" + imageName + "_forward.png").convert()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        ghost_colours.append(imageName)
         self.rect = self.surf.get_rect(
             center = (
-                random.randint(0, sw),
-                random.randint(0, sh)
+                714,
+                299
             )
         )
-    
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= sh:
-            self.rect.bottom = sh
 
-def move_pacman():
-    if K_a == True:
-        pac_man_surface = pygame.image.load("./Assets/Sprites/Pac_mans/Right_open.png").convert_alpha()
-        pac_man_surface.centerx -= 1
-    elif K_w == True:
-        pac_man_surface = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
-        pac_man_surface.centery -= 1
-    elif K_s == True:
-        pac_man_surface = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
-        pac_man_surface.centery += 1
-    elif K_d == True:
-        pac_man_surface = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
-        pac_man_surface.centerx += 1
-        
+def release_ghost():
+    pass
+
+class Pac_man(pygame.sprite.Sprite):
+    def move_pacman(self):
+        if K_LEFT == True:
+            self = pygame.image.load("./Assets/Sprites/Pac_mans/Right_open.png").convert_alpha()
+            self.rect.move_ip(-5, 0)
+        elif K_UP == True:
+            self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
+            self.rect.move_ip(0, -5)
+        elif K_DOWN == True:
+            self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
+            self.rect.move_ip(0, 5)
+        elif K_RIGHT == True:
+            self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()  
+            self.rect.move_ip(5, 0)
+   
+    def kill_pacman(surface,sounde,ghost):
+        pass
 
 pygame.init()
 
@@ -51,7 +49,10 @@ all_sprites = pygame.sprite.Group()
 ADDGHOST = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDGHOST, 1200)
 
-sw = 1427
+RELEASEGHOST = pygame.USEREVENT + 1
+pygame.time.set_timer(RELEASEGHOST, 5500)
+
+sw = 1428
 sh = 598
 screen = pygame.display.set_mode((sw,sh))
 
@@ -61,16 +62,24 @@ pac_man_rect = pac_man_surface.get_rect()
 
 
 
+# Game Variables
+ghost_amount = 0
+ghost_colours = []
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            move_pacman()
+            move_pacman(pac_man_surface)
         elif event.type == ADDGHOST:
-            ghost = Ghost()
-            all_sprites.add(ghost)
+            if ghost_amount < 4:
+                ghost = Ghost()
+                all_sprites.add(ghost)
+            ghost_amount += 1
+        elif event.type == RELEASEGHOST:
+            release_ghost()
 
     # Fill screen with black
     screen.fill((0, 0, 0))
