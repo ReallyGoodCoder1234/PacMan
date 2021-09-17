e0e789dc5ec38b40de3bc8af3d2cb4877e68ba99
ReallyGoodCoder1234 <andydai.inbox@gmail.com>
2021-09-18 09:38:07 +1000

    I have ,ade it a bit more tideir

--- Main.py
+++ Main.py
@@ -1,26 +1,11 @@
 import pygame, sys, random
-
 from pygame.cursors import tri_left
-from Buttons import Button
-
 from pygame.locals import (K_DOWN, K_UP, K_UP, K_LEFT, K_RIGHT, K_a, K_d, K_s, K_w, RLEACCEL)
 
-def move_pacman(self):
-    if K_LEFT == True:
-        self = pygame.image.load("./Assets/Sprites/Pac_mans/Right_open.png").convert_alpha()
-        self.rect.move_ip(-5, 0)
-    elif K_UP == True:
-        self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
-        self.rect.move_ip(0, -5)
-    elif K_DOWN == True:
-        self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()
-        self.rect.move_ip(0, 5)
-    elif K_RIGHT == True:
-        self = pygame.image.load("./Assets/Sprites/Pac_mans/Left_open.png").convert_alpha()  
-        self.rect.move_ip(5, 0)
-   
-def kill_pacman(surface,sounde,ghost):
-    pass
+from Buttons import Button
+from Ghost import Ghost
+from Pac_man import Pac_man
+
 
 pygame.init()
 
@@ -41,16 +26,17 @@ gui_font = pygame.font.Font(None,30)
 game_started = False
 credits_on = False
 Howtoplay = False
+escape = False
 
 # Buttons
 quit_button = Button('Quit',200,40,(25,450),6)
 credits_button = Button('Credits',200,40,(25,350),6)
 how_button = Button('How To Play',200,40,(275,450),6)
 play_button = Button('Play',200,40,(275,350),6)
+escape_button  = Button('Escape',200,40,(0,0),6)
 
 # Backgrounds
 main_menu_background = pygame.transform.scale2x(pygame.image.load("./Assets/Levels_and_backgrounds/Main_menu_backgound.png"))
-
 
 # Game loop
 while True:
@@ -60,30 +46,25 @@ while True:
            pygame.quit()
            sys.exit()
 
-        elif play_button.pressed == True:
+        if play_button.pressed == True:
             sw = 1428 
             sh = 598
             screen = pygame.display.set_mode((sw,sh))
             game_started = True
 
-        elif game_started == False and credits_on == False and Howtoplay == False:
+        if game_started == False and credits_on == False and Howtoplay == False:
             screen.blit(main_menu_background,(0,0))
             quit_button.draw(screen)
             credits_button.draw(screen)
             how_button.draw(screen)
             play_button.draw(screen)
+            escape = False
 
-        elif how_button.pressed == True:
+
+        if how_button.pressed == True:
             Howtoplay == True
 
-        elif credits_button.pressed == True:
-            credits_on == True
-
-        elif credits_on == True:
-            credits_text = gui_font.render('All credits go to ReallyGoodCoder1234 and MysteryCoder', True)
-            credits_text_rect = credits_text.get_rect()
-
-        elif Howtoplay == True:
+        if Howtoplay == True:
             how_text_level_1 = gui_font.render('To play press arrow keys:', True)
             how_text_level_2 = gui_font.render('Right key makes you move right,', True)
             how_text_level_3 = gui_font.render('Left key makes you move Left,', True)
@@ -92,14 +73,35 @@ while True:
             how_text_level_6 = gui_font.render('You cannot go through walls.', True)
             how_text_level_7 = gui_font.render('You also cannot eat ghosts unless', True)
             how_text_level_8 = gui_font.render('you have eaten a flashing pink dot', True)
-            how_text_rect = how_text_level_1.get_rect()
-            how_text_rect = how_text_level_2.get_rect()
-            how_text_rect = how_text_level_3.get_rect()
-            how_text_rect = how_text_level_4.get_rect()
-            how_text_rect = how_text_level_5.get_rect()
-            how_text_rect = how_text_level_6.get_rect()
-            how_text_rect = how_text_level_7.get_rect()
-            how_text_rect = how_text_level_8.get_rect()
+            how_text_rect_1 = how_text_level_1.get_rect()
+            how_text_rect_2 = how_text_level_2.get_rect()
+            how_text_rect_3 = how_text_level_3.get_rect()
+            how_text_rect_4 = how_text_level_4.get_rect()
+            how_text_rect_5 = how_text_level_5.get_rect()
+            how_text_rect_6 = how_text_level_6.get_rect()
+            how_text_rect_7 = how_text_level_7.get_rect()
+            how_text_rect_8 = how_text_level_8.get_rect()
+        
+        if credits_button.pressed == True:
+            credits_on == True
+            
+        if credits_on == True:
+            credits_text = gui_font.render('All credits go to ReallyGoodCoder1234 and MysteryCoder', True)
+            credits_text_rect = credits_text.get_rect()
+        
+        if Howtoplay == True:
+            screen.blit(how_text_level_1,how_text_rect_1)
+            screen.blit(how_text_level_1,how_text_rect_2)
+            screen.blit(how_text_level_1,how_text_rect_3)
+            screen.blit(how_text_level_1,how_text_rect_4)
+            screen.blit(how_text_level_1,how_text_rect_5)
+            screen.blit(how_text_level_1,how_text_rect_6)
+            screen.blit(how_text_level_1,how_text_rect_7)
+            screen.blit(how_text_level_1,how_text_rect_8)
+
+        if credits_on == True:
+            screen.blit(credits_text,credits_text_rect)
+
             how_text_level_1.center = (())
         #if event.type == pygame.KEYDOWN:
         #    move_pacman(pac_man_surface)
