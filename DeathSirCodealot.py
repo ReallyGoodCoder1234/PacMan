ed1e63a8604b5865f270d03d40f19646c33ca82d
ReallyGoodCoder1234 <andydai.inbox@gmail.com>
2022-11-04 20:16:09 +1100

    Fixed bug issues and added a Readme

--- DeathSirCodealot.py
+++ DeathSirCodealot.py
@@ -3,3 +3,9 @@ import pygame
 class SirCodealot(pygame.sprite.Sprite):
     def __init__(self, height, width):
         super(SirCodealot, self).__init__()
+        image = "Assets/Levels_and_backgrounds/FaceRight.png"
+        self.surf = pygame.image.load(image).convert_alpha()
+        pygame.transform.scale2x(self.surf)
+        self.rect = self.surf.get_rect(
+            center = (width // 2, height // 2)
+        )
